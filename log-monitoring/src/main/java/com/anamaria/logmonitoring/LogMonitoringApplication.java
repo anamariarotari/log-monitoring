86777469b38553c05e72a1cb6115e7d09cc62069
arotari <anamaria.rotari25@gmail.com>
2025-03-25 12:23:26 +0200

    Add main application entrypoint, added example logs.log input and generate output.log report

--- log-monitoring/src/main/java/com/anamaria/logmonitoring/LogMonitoringApplication.java
+++ log-monitoring/src/main/java/com/anamaria/logmonitoring/LogMonitoringApplication.java
@@ -1,7 +1,17 @@
 package com.anamaria.logmonitoring;
 
+import java.util.ArrayList;
+
 public class LogMonitoringApplication {
+    private static final String INPUT_FILE = "logs.log";
+    private static final String OUTPUT_FILE = "output.log";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var warnings = new ArrayList<String>();
+        var errors = new ArrayList<String>();
+
+        new LogProcessor().processLogs(INPUT_FILE, warnings, errors);
+        LogFileWriter.writeReport(OUTPUT_FILE, warnings, errors);
+        System.out.println("Log processing complete. Report saved to: " + OUTPUT_FILE);
     }
 }
